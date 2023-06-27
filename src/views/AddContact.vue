78e803fa5572d6cb0b9612cda9847f2e5333d8ef
Aashani123 <ranasingharaat.20@uom.lk>
2023-06-27 22:13:23 +0530

    updated

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -1,6 +1,45 @@
 <template>
-    <div class="about">
-      <h1>This is an Add page</h1>
+   <div class="container mt-3">
+     <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Add Contact</p>
+        <p class="fst-italic">Welcome to the Add Contact page of our Contact Management System! This user-friendly interface allows you to effortlessly add new contacts to your database. Whether you're looking to store personal or professional information, our system makes it easy to organize and manage your contacts effectively. Simply fill in the required fields with the contact's details, and with just a click, their information will be seamlessly added to your collection. Stay connected and keep your contacts organized with our efficient Add Contact feature.</p>
+      </div>
+     </div>
+   </div>
+   <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-4">
+        <form>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Name">
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Picture URL">
+          </div>
+          <div class="mb-2">
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="mb-2">
+            <input type="number" class="form-control" placeholder="Mobile">
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Company">
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Title">
+          </div>
+          <div class="mb-2">
+            <select class="form-control">
+              <option value="">Select Group</option>
+            </select> 
+          </div>
+          <div class="mb-2">
+            <input type="submit" class="btn btn-success" value="Create">
+          </div>
+        </form>  
+      </div>
     </div>
+   </div>
   </template>
   
--- src/views/ContactManager.vue.vue
+++ src/views/ContactManager.vue.vue
@@ -6,6 +6,55 @@
                 <router-link to="/contacts/add" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i>  New</router-link>
             </p>
             <p class="fst-italic">A contact management system is a powerful tool that helps individuals and businesses efficiently organize and maintain their contacts. With this system, users can store and manage a vast array of contact information, such as names, addresses, phone numbers, and email addresses, all in one centralized location. </p>
+          <form>
+            <div class="row">
+               <div class="col-md-6">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Search Name">
+                    </div>
+                    <div class="col">
+                        <input type="submit" class="btn btn-outline-dark">
+                    </div>
+                </div>
+               </div>
+            </div>
+          </form>
+        </div>
+    </div>
+   </div>
+
+   <!-- create card -->
+   <div class="container mt-3">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card my-2 list-group-item-succes shadow">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-sm-4">
+                        <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" class="contact-img">
+                        </div>
+                        <div class="col-sm-7">
+                          <ul class="list-group">
+                            <li class="list-group-item">Name: <span class="fw-bold">Name</span></li>
+                            <li class="list-group-item">Email: <span class="fw-bold">Email</span></li>
+                            <li class="list-group-item">Mobile No: <span class="fw-bold">Modile No</span></li>
+
+                          </ul>
+                        </div>
+                        <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
+                           <router-link to="/contacts/view/:contactId" class="btn btn-warning my-1">
+                            <i class="fa fa-eye"></i>
+                           </router-link>
+                           <router-link to="/contacts/edit/:contactId" class="btn btn-primary my-1">
+                            <i class="fa fa-pen"></i></router-link>
+                          <button class="btn btn-danger my-1">
+                            <i class="fa fa-trash"></i>
+                          </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
    </div>
